eae46dfda7949f383677a8f53974b836f3f4c8c1
RommelJoshuaDiaz <35924805+RommelJoshuaDiaz@users.noreply.github.com>
2018-03-10 14:39:58 +0800

    Pacheck
    try.

---
+++ AndroidEatIt/app/src/main/java/com/example/romme/androideatit/Interface/itemClickListener.java
@@ -0,0 +1,13 @@
+package com.example.romme.androideatit.Interface;
+
+import android.view.View;
+
+/**
+ * Created by romme on 09/03/2018.
+ */
+
+public interface itemClickListener {
+
+    void onItemClick(View view,int position,boolean isLongClick);
+
+}
---
+++ AndroidEatIt/app/src/main/java/com/example/romme/androideatit/ViewHolder/MenuViewHolder.java
@@ -0,0 +1,47 @@
+package com.example.romme.androideatit.ViewHolder;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.romme.androideatit.Model.Category;
+import com.example.romme.androideatit.R;
+
+/**
+ * Created by romme on 09/03/2018.
+ */
+
+public class MenuViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+    public TextView txtMenuName;
+    public ImageView imageView;
+
+    private AdapterView.OnItemClickListener itemClickListener;
+
+    public MenuViewHolder(View itemView) {
+        super(itemView);
+
+        txtMenuName = (TextView) itemView.findViewById(R.id.menu_name);
+        imageView = (ImageView) itemView.findViewById(R.id.menu_image);
+
+        itemView.setOnClickListener(this);
+
+    }
+
+    public void setItemClickListener(AdapterView.OnItemClickListener itemClickListener) {
+        this.itemClickListener = itemClickListener;
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    public void onItemClick(AdapterView<?> adapterView, View view, int position, long id ) {
+        itemClickListener.onItemClick(adapterView,view,getAdapterPosition(),id);
+
+    }
+}
